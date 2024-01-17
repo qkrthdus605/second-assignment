3f2cb4f1405ce2c2cfddf7968c68c447188c307f
qkrthdus605 <qkrthdus605@gmail.com>
2024-01-17 17:45:57 +0900

    style: 이미지 및 전역 스타일링 추가

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -3,13 +3,20 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
-    *{
+    * {
         marign: 0;
         padding: 0;
         box-sizing: border-box;
     }
-    body{
+
+    body {
         background-color: gray;
+    }
+
+    div, p{
+        color: black;
+        font-size: 18px;
+        font-weight: 400;    
     }
 `;
 
